eb3eeed38b6e85617dc7fc25a4bca1433c7a9ed3
Richard Li <lichard49@gmail.com>
2013-02-08 15:40:33 -0500

    Spaces

--- src/com/example/where_is_my_stuff/MainActivity.java
+++ src/com/example/where_is_my_stuff/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		
-		System.out.println("Activity started!");
+		System.out.println("Activity started!");   
     }
 
     @Override
